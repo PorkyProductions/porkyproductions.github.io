55a9c579427ba1e8ab0926b8e324dc87afcf02bf
hiteacheryouare <82683251+hiteacheryouare@users.noreply.github.com>
2022-01-13 17:58:39 -0500

    added firefox to the list

--- js/Utility.js
+++ js/Utility.js
@@ -22,7 +22,7 @@ const DetectDeviceType = () => {
     if (/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(ua)) {
         return "tablet";
     }
-    else if (/Mobile|Android|iP(hone|od)|IEMobile|BlackBerry|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(ua)) {
+    else if (/Mobile|Android|iP(hone|od)|IEMobile|BlackBerry|Kindle|Silk-Accelerated|(hpw|web)OS|Firefox|Firefox M(obi|Opera M(obi|ini)/.test(ua)) {
         return "mobile";
     }
     return "desktop";
