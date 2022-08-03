797daac2936fb3ba2ce11a537e4abc311ae0ae83
hiteacheryouare <82683251+hiteacheryouare@users.noreply.github.com>
2022-08-03 16:15:40 -0400

    feat: :sparkles: made the hero

--- src/components/hero.svelte
+++ src/components/hero.svelte
@@ -4,9 +4,13 @@ import HeroTypewriter from './heroTypewriter.svelte';
 </script>
 
 
-<hero class="bg-gradient-to-br from-primary to-ppio-hotshot">
-    <div class="bg-gradient-to-br from-primary to-ppio-hotshot flex justify-between content-center p-20">
-        <HeroTypewriter />
-        <img src="../../images/HedgehogIcon.png" alt="">
+<hero class="bg-gradient-to-br from-primary to-ppio-hotshot dark:from-secondary-dark dark:to-secondary-darkest">
+    <div class="bg-gradient-to-br from-primary to-ppio-hotshot flex justify-between content-center dark:from-secondary-dark dark:to-secondary-darkest p-48">
+        <div class="m-auto">
+            <HeroTypewriter />
+        </div>
+        <div class="m-auto">
+            <img src="../../images/HedgehogIcon.png" alt="The PorkyProductions Logo">
+        </div>
     </div>
 </hero>
--- src/components/heroTypewriter.svelte
+++ src/components/heroTypewriter.svelte
@@ -22,7 +22,7 @@
         }
     </script>
     
-    <main>
+    <main class="pl-3">
         <div id="bg" class="p-10">
             <p class="text-white text-2xl">
                 Hi, we're <Typewriter
@@ -30,19 +30,19 @@
             --cursor-color={props["--cursor-color"]}
             on:done={() => console.log("The animation has finished!")}
             {...props}>
-            <h3 class="font-secondary font-bold text-white sm:text-xl md:text-4xl lg:text-5xl xl:text-7xl">
+            <h3 class="font-secondary font-bold text-white sm:text-3xl md:text-5xl lg:text-6xl xl:text-8xl">
                 PorkyProductions
             </h3>
-            <h3 class="font-secondary font-bold text-white sm:text-2xl md:text-4xl lg:text-5xl xl:text-7xl">
+            <h3 class="font-secondary font-bold text-white sm:text-3xl md:text-5xl lg:text-6xl xl:text-8xl">
                 Making Games
             </h3>
-            <h3 class="font-secondary font-bold text-white sm:text-2xl md:text-4xl lg:text-5xl xl:text-7xl">
+            <h3 class="ffont-secondary font-bold text-white sm:text-3xl md:text-5xl lg:text-6xl xl:text-8xl">
                 Developing Software
             </h3>
             <h3 class="font-secondary font-bold text-white sm:text-3xl md:text-5xl lg:text-6xl xl:text-8xl">
                 3D Printing
             </h3>
-            <h3 class="font-secondary font-bold text-white sm:text-2xl md:text-4xl lg:text-5xl xl:text-7xl">
+            <h3 class="font-secondary font-bold text-white sm:text-3xl md:text-5xl lg:text-6xl xl:text-8xl">
                 Pushing What's Possible
             </h3>
             <h3 class="font-secondary font-bold text-white sm:text-3xl md:text-5xl lg:text-6xl xl:text-8xl">
@@ -59,6 +59,14 @@
             box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.37 );
             backdrop-filter: blur( 15px );
             -webkit-backdrop-filter: blur( 15px );
+            border-radius: 10px;
+            border: 1px solid rgba( 255, 255, 255, 0.18 );
+        }
+        #bg:hover {
+            background: rgba( 74, 144, 226, 0.2 );
+            box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.37 );
+            backdrop-filter: blur( 45px );
+            -webkit-backdrop-filter: blur( 45px );
             border-radius: 10px;
             border: 1px solid rgba( 255, 255, 255, 0.18 );
         }
