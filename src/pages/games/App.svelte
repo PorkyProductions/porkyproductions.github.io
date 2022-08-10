6b0dbb0d6b0ce553a2c3319218ee92eb2cd23520
hiteacheryouare <82683251+hiteacheryouare@users.noreply.github.com>
2022-08-09 21:37:15 -0400

    Update App.svelte

--- src/pages/games/App.svelte
+++ src/pages/games/App.svelte
@@ -6,19 +6,3 @@
 
 <Header />
 <GamesPageMain />
-
-
-
-<style>
-.grid {
-    display:grid;
-    grid-template-columns:1fr 1fr
-}
-.tile {
-    padding:50px;
-    margin:50px;
-    border:5px solid hsl(240, 100%, 48%);
-    border-radius:30px;
-    text-align:left;
-}
-</style>
