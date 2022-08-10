a367f7917db68db4014d2af5932565799897ad50
hiteacheryouare <82683251+hiteacheryouare@users.noreply.github.com>
2022-08-09 21:28:10 -0400

    games page
    Co-authored-by: Tristan Winata <HedgehogDubz@users.noreply.github.com>

---
+++ src/components/gamesPageMain.svelte
@@ -0,0 +1,32 @@
+<script>
+    import '../app.css'
+</script>
+
+<section>
+    <h1 class="text-center font-primary font-semibold text-black sm:text-lg md:text-xl lg:text-2xl xl:text-3xl p-6">
+        Hedgehog Studios Games
+    </h1>
+    <div class="grid">
+        <div class="tile">
+
+        </div>
+        <div class="tile">
+
+        </div>
+    </div>
+</section>
+
+<style>
+.grid {
+    display:grid;
+    grid-template-columns:1fr 1fr
+}
+.tile {
+    padding:50px;
+    margin:50px;
+    border:5px solid hsl(240, 100%, 48%);
+    border-radius:30px;
+    text-align:left;
+}
+</style>
+
---
+++ src/pages/games/App.svelte
@@ -0,0 +1,24 @@
+<script>
+    import GamesPageMain from '../../components/gamesPageMain.svelte';
+    import Header from '../../components/header.svelte';
+    import '../../app.css'
+</script>
+
+<Header />
+<GamesPageMain />
+
+
+
+<style>
+.grid {
+    display:grid;
+    grid-template-columns:1fr 1fr
+}
+.tile {
+    padding:50px;
+    margin:50px;
+    border:5px solid hsl(240, 100%, 48%);
+    border-radius:30px;
+    text-align:left;
+}
+</style>
