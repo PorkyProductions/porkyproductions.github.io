3727076465d5de2b282c336f75dfd478ff933a58
HedgehogDubz <HedgehogDubz@users.noreply.github.com>
2022-08-10 18:51:18 -0400

    header looks better 1

--- src/components/header.svelte
+++ src/components/header.svelte
@@ -17,51 +17,64 @@
 
 
 <header class="bg-slate-200 dark:bg-slate-700 dark:text-white">
-    <a href="index.html" class="hover:bg-slate-300 dark:hover:bg-slate-800 hover:rounded-xl rounded-lg"><img src={hedgehog} alt="a hedgehog" id="HH" on:mouseover={spin} on:focus={spin}></a>
+    
     {#if DEVICE_type === "mobile"}
         <div></div>
     {:else if DEVICE_type === "desktop"}
+    
         <div class="flex content-center justify-between p-4">
-            
-            <div class="flex justify-between content-center p-2">
+          <div></div>
+          
+          <a href="index.html" class="hover:bg-slate-300 dark:hover:bg-slate-800 hover:rounded-xl rounded-lg">
+            <img src={hedgehog} alt="a hedgehog" id="HH" on:mouseover={spin} on:focus={spin}>
+          </a>
+
+          <div class="flex justify-between content-center p-2">
                 <a href="index.html" class="sm:text-sm md:text-md lg:text-lg xl:text-xl font-primary font-medium hover:bg-primary hover:text-white hover:font-semibold dark:hover:bg-slate-800 hover:rounded-xl">
                     Home
+                    <img src = {houseDoor} alt ="house">
                 </a>
                 <br>
-                <img src={houseDoor} alt="controller">
             </div>
             <div class="flex justify-between content-center p-2">
                 <a href="games.html" class="sm:text-sm md:text-md lg:text-lg xl:text-xl font-primary font-medium hover:bg-primary hover:text-white hover:font-semibold dark:hover:bg-slate-800 hover:rounded-xl">
                     Games 
+                    <img src = {controller} alt ="controller">
+
                 </a>
                 <br>
-                <img src={controller} alt="controller">
             </div>
             <div class="flex justify-between content-center p-2">
                 <a href="threeDprints.html" class="sm:text-sm md:text-md lg:text-lg xl:text-xl font-primary font-medium hover:bg-primary hover:text-white hover:font-semibold dark:hover:bg-slate-800 hover:rounded-xl">
-                    3D Prints
+                  3D Prints
+                  <img src ={badge3d} alt ="3d">
+
                 </a>
-                <img src={badge3d} alt="">
             </div>
             <div class="flex justify-between content-center p-2">
                 <a href="models.html" class="sm:text-sm md:text-md lg:text-lg xl:text-xl font-primary font-medium hover:bg-primary hover:text-white hover:font-semibold dark:hover:bg-slate-800 hover:rounded-xl">
-                    Models
+                  Models
+                  <img src = {boxes} alt ="box">
+
                 </a>
-                <img src={boxes} alt="boxes">
             </div>
             <div class="flex justify-between content-center p-2">
                 <a href="developments.html" class="sm:text-sm md:text-md lg:text-lg xl:text-xl font-primary font-medium hover:bg-primary hover:text-white hover:font-semibold dark:hover:bg-slate-800 hover:rounded-xl">
-                    Developments
+                  Developments
+                  <img src={codeSlash} alt="code">
+
                 </a>
-                <img src={codeSlash} alt="">
-            </div>
+              </div>
+          <div></div>
+          <div></div>
+          <div></div>
         </div>
     {/if}
 </header>
 
 <style>
     #HH {
-        width: 5%;
+        width: 35%;
     }
     header {
         position: sticky;
@@ -74,5 +87,9 @@
     @keyframes spin {
         0% { transform: rotate(0deg); }
         100% { transform: rotate(360deg); }
+    }
+    a:hover {
+    color:black;
+     background-color: lightgray;
     }
 </style>
