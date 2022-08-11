d12c573def276d0d17c5f132713f34077ec5b4b7
hiteacheryouare <82683251+hiteacheryouare@users.noreply.github.com>
2022-08-10 22:12:47 -0400

    header becomes gooder once more

--- src/components/header.svelte
+++ src/components/header.svelte
@@ -1,87 +1,96 @@
 <script>
-  // https://vitejs.dev/guide/assets.html
-  import '../app.css'
-  import { DEVICE_type } from 'uadetect'
-  import controller from '../../node_modules/bootstrap-icons/icons/controller.svg'
-  import boxes from '../../node_modules/bootstrap-icons/icons/boxes.svg'
-  import badge3d from '../../node_modules/bootstrap-icons/icons/badge-3d.svg'
-  import codeSlash from '../../node_modules/bootstrap-icons/icons/code-slash.svg'
-  import hedgehog from '/./../images/HedgehogIcon.png'
-  import houseDoor from '../../node_modules/bootstrap-icons/icons/house-door.svg'
-
-  const HH = document.getElementById("HH")
-  const spin = () => {
-  HH.classList.add('spin');
-  }
-</script>
-
-
-<header class="bg-slate-200 dark:bg-slate-700 dark:text-white">
-    
-    {#if DEVICE_type === "mobile"}
-        <div></div>
-    {:else if DEVICE_type === "desktop"}
-    
-        <div class="flex content-center justify-between p-4">
-            <div></div>
-            
-            <a href="index.html" class="hover:bg-slate-300 dark:hover:bg-slate-800 hover:rounded-xl rounded-lg">
-                <img src={hedgehog} alt="a hedgehog" id="HH" on:mouseover={spin} on:focus={spin}>
-            </a>
-
-            <div class="flex justify-between content-center p-2">
-                <a href="index.html" class="sm:text-sm md:text-md lg:text-lg xl:text-xl font-primary font-medium hover:bg-slate-500 hover:text-white hover:font-semibold dark:hover:bg-slate-800 hover:rounded-xl">
-                    Home
-                    <img src = {houseDoor} alt ="house">
-                </a>
-                <br>
-            </div>
-            <div class="flex justify-between content-center p-2">
-                <a href="games.html" class="sm:text-sm md:text-md lg:text-lg xl:text-xl font-primary font-medium hover:bg-slate-500 hover:text-white hover:font-semibold dark:hover:bg-slate-800 hover:rounded-xl">
-                    Games 
-                    <img src = {controller} alt ="controller">
-                </a>
-                <br>
-            </div>
-            <div class="flex justify-between content-center p-2">
-                <a href="threeDprints.html" class="sm:text-sm md:text-md lg:text-lg xl:text-xl font-primary font-medium hover:bg-slate-500 hover:text-white hover:font-semibold dark:hover:bg-slate-800 hover:rounded-xl">
-                    3D Prints
-                    <img src ={badge3d} alt ="3d">
-                </a>
-            </div>
-            <div class="flex justify-between content-center p-2">
-                <a href="models.html" class="sm:text-sm md:text-md lg:text-lg xl:text-xl font-primary font-medium hover:bg-slate-500 hover:text-white hover:font-semibold dark:hover:bg-slate-800 hover:rounded-xl">
-                    Models
-                    <img src = {boxes} alt ="box">
-                </a>
-            </div>
-            <div class="flex justify-between content-center p-2">
-                <a href="developments.html" class="sm:text-sm md:text-md lg:text-lg xl:text-xl font-primary font-medium hover:bg-slate-500 hover:text-white hover:font-semibold dark:hover:bg-slate-800 hover:rounded-xl">
-                    Developments
-                    <img src={codeSlash} alt="code">
-                </a>
-            </div>
-        <div></div>
-        <div></div>
-        <div></div>
-        </div>
-    {/if}
-</header>
-
-<style>
-    #HH {
-        width: 35%;
+    // https://vitejs.dev/guide/assets.html
+    import '../app.css'
+    import { DEVICE_type } from 'uadetect'
+    import controller from '../../node_modules/bootstrap-icons/icons/controller.svg'
+    import boxes from '../../node_modules/bootstrap-icons/icons/boxes.svg'
+    import badge3d from '../../node_modules/bootstrap-icons/icons/badge-3d.svg'
+    import codeSlash from '../../node_modules/bootstrap-icons/icons/code-slash.svg'
+    import hedgehog from '/./../images/HedgehogIcon.png'
+    import houseDoor from '../../node_modules/bootstrap-icons/icons/house-door.svg'
+  
+    const HH = document.getElementById("HH")
+    const spin = () => {
+    HH.classList.add('spin');
     }
-    header {
-        position: sticky;
-        top: 0;
-        z-index: 5;
-    }
-    .spin {
-        animation: spin 2s infinite cubic-bezier(0.96, 0.95, 0, 1.22);
-    }
-    @keyframes spin {
-        0% { transform: rotate(0deg); }
-        100% { transform: rotate(360deg); }
-    }
-</style>
+  </script>
+  
+  
+  <header class="bg-slate-200 dark:bg-slate-700 dark:text-white">
+      
+      {#if DEVICE_type === "mobile"}
+          <div></div>
+      {:else if DEVICE_type === "desktop"}
+      
+          <div class="flex content-center justify-between p-4">
+              <div></div>
+              
+              <a href="index.html" class="header-link hover:bg-slate-300 dark:hover:bg-slate-800 hover:rounded-xl rounded-lg">
+                  <img src={hedgehog} alt="a hedgehog" id="HH" on:mouseover={spin} on:focus={spin}>
+              </a>
+  
+              <div class="flex justify-between content-center p-2">
+                  <a href="index.html" class="header-link sm:text-sm md:text-md lg:text-lg xl:text-xl font-primary font-medium hover:bg-slate-400 hover:font-semibold  hover:rounded-xl">
+                      Home
+                      <img src = {houseDoor} alt ="house">
+                  </a>
+                  <br>
+              </div>
+              <div class="flex justify-between content-center p-2">
+                  <a href="games.html" class="header-link sm:text-sm md:text-md lg:text-lg xl:text-xl font-primary font-medium hover:bg-slate-400  hover:font-semibold dark:hover:bg-slate-800 hover:rounded-xl">
+                      Games 
+                      <img src = {controller} alt ="controller">
+                  </a>
+                  <br>
+              </div>
+              <div class="flex justify-between content-center p-2">
+                  <a href="threeDprints.html" class="header-link sm:text-sm md:text-md lg:text-lg xl:text-xl font-primary font-medium hover:bg-slate-400  hover:font-semibold dark:hover:bg-slate-800 hover:rounded-xl">
+                      3D Prints
+                      <img src ={badge3d} alt ="3d">
+                  </a>
+              </div>
+              <div class="flex justify-between content-center p-2">
+                  <a href="models.html" class="header-link sm:text-sm md:text-md lg:text-lg xl:text-xl font-primary font-medium hover:bg-slate-400  hover:font-semibold dark:hover:bg-slate-800 hover:rounded-xl">
+                      Models
+                      <img src = {boxes} alt ="box">
+                  </a>
+              </div>
+              <div class="flex justify-between content-center p-2">
+                  <a href="developments.html" class="header-link sm:text-sm md:text-md lg:text-lg xl:text-xl font-primary font-medium hover:bg-slate-400  hover:font-semibold dark:hover:bg-slate-800 hover:rounded-xl">
+                      Developments
+                      <img src={codeSlash} alt="code">
+                  </a>
+              </div>
+          <div></div>
+          <div></div>
+          <div></div>
+          </div>
+      {/if}
+  </header>
+  
+  <style>
+      #HH {
+          width: 35%;
+      }
+      header {
+          position: sticky;
+          top: 0;
+          z-index: 5;
+      }
+      .spin {
+          animation: spin 2s infinite cubic-bezier(0.96, 0.95, 0, 1.22);
+      }
+      @keyframes spin {
+          0% { transform: rotate(0deg); }
+          100% { transform: rotate(360deg); }
+      }
+      .header-link {
+          padding:20px;
+          margin-top: -15px;
+          margin-bottom:-15px;
+          padding-right: 50px;
+          padding-left:50px;
+          
+      }
+      
+  </style>
