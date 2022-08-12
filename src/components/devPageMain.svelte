1d6d27ea4866fa09df3dd4eecca6124f4630cca9
hiteacheryouare <82683251+hiteacheryouare@users.noreply.github.com>
2022-08-11 21:02:57 -0400

    feat: :zap: finished developments page

---
+++ src/components/devPageMain.svelte
@@ -0,0 +1,36 @@
+<script>
+    import '../app.css'
+</script>
+
+<section class="bg-white text-black dark:text-white dark:bg-black">
+    <h1 class="text-center font-primary font-semibold sm:text-lg md:text-xl lg:text-2xl xl:text-3xl p-6">
+        Technological Developments
+    </h1>
+    <p class="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus facilis perferendis ullam dicta et laborum dolorum nobis omnis alias rem. Accusamus itaque aliquam, eaque temporibus alias blanditiis hic dignissimos cum.</p>
+    <div class="bg-gradient-to-r from-primary to-blue-400 hover:bg-slate-600 p-10">
+        <h1 class="text-center font-semibold p-24 text-white sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-primary">
+            <a href="https://porkyproductions.github.io/deMotivator/">(de)Motivator</a>
+        </h1>
+    </div>  
+    <div class="bg-gradient-to-r from-UADblue to-UADGreen hover:bg-slate-600 p-10">
+        <h1 class="text-center font-semibold p-24 text-white sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-primary">
+            <a href="https://porkyproductions.github.io/UA-detect/">
+                UADetect
+            </a>
+        </h1>
+    </div>
+    <div class="bg-gradient-to-r from-orange-600 to-yellow-400 hover:bg-slate-600 p-10">
+        <h1 class="text-center font-semibold p-24 text-white sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-primary">
+            <a href="https://www.npmjs.com/package/demotivator">(de)Motivator.js</a>
+        </h1>
+    </div>
+    <div class="bg-gradient-to-r from-black to-slate-800 hover:bg-slate-600 p-10">
+        <h1 class="text-center font-semibold p-24 text-white sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-primary">
+            Coming Soon
+        </h1>
+    </div>
+</section>
+
+<style>
+
+</style>
---
+++ src/pages/developments/App.svelte
@@ -0,0 +1,10 @@
+<script>
+    import Header from '../../components/header.svelte';
+    import '../../app.css'
+    import Footer from '../../components/footer.svelte';
+    import DevPageMain from '../../components/devPageMain.svelte';
+</script>
+
+<Header />
+<DevPageMain />
+<Footer />
