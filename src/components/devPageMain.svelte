ba3122ac9b96b602bdf6433763ab613bdd7839cf
hiteacheryouare <82683251+hiteacheryouare@users.noreply.github.com>
2022-08-11 21:09:55 -0400

    added lorem to dev page

--- src/components/devPageMain.svelte
+++ src/components/devPageMain.svelte
@@ -11,6 +11,7 @@
         <h1 class="text-center font-semibold p-24 text-white sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-primary">
             <a href="https://porkyproductions.github.io/deMotivator/">(de)Motivator</a>
         </h1>
+        <p class="text-center font-normal p-4 text-white sm:text-lg md:text-xl lg:text-2xl xl:text-3xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut explicabo, iste, tempora deleniti tempore nam neque nemo mollitia, fugit veritatis omnis. Similique quasi ab saepe distinctio illum optio possimus beatae?</p>
     </div>  
     <div class="bg-gradient-to-r from-UADblue to-UADGreen hover:bg-slate-600 p-10">
         <h1 class="text-center font-semibold p-24 text-white sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-primary">
@@ -18,16 +19,19 @@
                 UADetect
             </a>
         </h1>
+        <p class="text-center font-normal p-4 text-white sm:text-lg md:text-xl lg:text-2xl xl:text-3xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus provident, vero necessitatibus sunt rerum laudantium numquam reiciendis explicabo eos, labore adipisci! Eius, quia repudiandae! Quibusdam facere eaque nulla sit itaque.</p>
     </div>
     <div class="bg-gradient-to-r from-orange-600 to-yellow-400 hover:bg-slate-600 p-10">
         <h1 class="text-center font-semibold p-24 text-white sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-primary">
             <a href="https://www.npmjs.com/package/demotivator">(de)Motivator.js</a>
         </h1>
+        <p class="text-center font-normal p-4 text-white sm:text-lg md:text-xl lg:text-2xl xl:text-3xl">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit doloremque tempora cum corrupti assumenda ratione eius consectetur, natus corporis in velit quod architecto dolorum laborum sunt? Recusandae cumque ex architecto.</p>
     </div>
     <div class="bg-gradient-to-r from-black to-slate-800 hover:bg-slate-600 p-10">
         <h1 class="text-center font-semibold p-24 text-white sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-primary">
             Coming Soon
         </h1>
+        <p class="text-center font-normal p-4 text-white sm:text-lg md:text-xl lg:text-2xl xl:text-3xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto sed nihil quidem ipsum, autem incidunt dolorum soluta dignissimos cupiditate corporis nostrum laboriosam iusto asperiores facilis, odit doloremque qui enim odio!</p>
     </div>
 </section>
 
