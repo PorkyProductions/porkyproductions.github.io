d2c8b49ad36749c94ab3292ebc1edbc5ea8312e5
HedgehogDubz <HedgehogDubz@users.noreply.github.com>
2022-08-12 00:32:36 -0400

    models page looks better but doesnt work yet

--- src/components/modelsPageMain.svelte
+++ src/components/modelsPageMain.svelte
@@ -1,7 +1,7 @@
 <script>
     import '../app.css'
     import icelands from '../../images/icelands.png'
-    import hedgehog from '../../images/hedgehogmodel.png'
+    import hedgehog from '../../images/hedgehogPreview_Front.png'
     import hedgehogModel from '../../models/hedgehog.stl'
     import { DEVICE_type } from 'uadetect'
 </script>
@@ -15,7 +15,10 @@
                 <h1 class="font-bold p-4 font-primary sm:text-md md:text-lg lg:text-xl xl:text-2xl">
                     Hedgehog
                 </h1>
-                <img src={hedgehog} alt="hedgehogModel" class="p-4">
+                <button class = "previewButton text-8xl">&lt;</button>
+                <img src={hedgehog} alt="hedgehogModel" class="previewImage p-4  w-3/4">   <!--ACTUAL IMAGE ACTUAL IMAGEACTUAL IMAGE ACTUAL IMAGEACTUAL IMAGE ACTUAL IMAGEACTUAL IMAGE ACTUAL IMAGE-->
+                <button class = "previewButton text-8xl">&gt;</button>
+
                 <p>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum iste molestias repellendus dolorum qui quia, iure fugiat in vel optio! Iusto, cumque distinctio delectus fuga repellat at accusamus quis repudiandae!
                 </p>
@@ -27,7 +30,9 @@
                 <h1 class="font-bold p-4 font-primary sm:text-md md:text-lg lg:text-xl xl:text-2xl">
                     Icelands
                 </h1>
-                <img src={icelands} alt="" class="p-4">
+                <button class = "previewButton text-8xl">&lt;</button>
+                <img src={icelands} alt="" class="previewImage p-4  w-3/4"><!--ACTUAL IMAGE ACTUAL IMAGEACTUAL IMAGE ACTUAL IMAGEACTUAL IMAGE ACTUAL IMAGEACTUAL IMAGE ACTUAL IMAGE-->
+                <button class = "previewButton text-8xl">&gt;</button>
                 <p>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, nam? Reprehenderit consequatur alias neque ullam error. Repellat beatae ex, ducimus corrupti nulla veritatis expedita unde excepturi aspernatur laboriosam provident officia.
                 </p>
@@ -46,8 +51,12 @@
                 <h1 class="font-bold p-4 font-primary sm:text-md md:text-lg lg:text-xl xl:text-2xl">
                     Hedgehog
                 </h1>
+
                 <div class="flex justify-center">
-                    <img src={hedgehog} alt="hedgehogModel" class="p-4">
+                    <button class = "previewButton text-8xl">&lt;</button>
+                    <img src={hedgehog} alt="hedgehogModelPreview" class="previewImage p-4 w-3/4"><!--ACTUAL IMAGE ACTUAL IMAGEACTUAL IMAGE ACTUAL IMAGEACTUAL IMAGE ACTUAL IMAGEACTUAL IMAGE ACTUAL IMAGE-->
+                    <button class = "previewButton text-8xl">&gt;</button>
+
                 </div>
                 <p>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum iste molestias repellendus dolorum qui quia, iure fugiat in vel optio! Iusto, cumque distinctio delectus fuga repellat at accusamus quis repudiandae!
@@ -61,7 +70,10 @@
                     Icelands
                 </h1>
                 <div class="flex justify-center">
-                <img src={icelands} alt="" class="p-4">
+                    <button class = "previewButton text-8xl">&lt;</button>
+                    <img src={icelands} alt="" class="previewImage p-4  w-3/4"><!--ACTUAL IMAGE ACTUAL IMAGEACTUAL IMAGE ACTUAL IMAGEACTUAL IMAGE ACTUAL IMAGEACTUAL IMAGE ACTUAL IMAGE-->
+                    <button class = "previewButton text-8xl">&gt;</button>
+
                 </div>
                 <p>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, nam? Reprehenderit consequatur alias neque ullam error. Repellat beatae ex, ducimus corrupti nulla veritatis expedita unde excepturi aspernatur laboriosam provident officia.
@@ -77,29 +89,32 @@
 {/if}
 
 <style>
-.grid {
-    display:grid;
-    grid-template-columns:1fr 1fr
-}
-.tile {
-    padding: 50px;
-    margin: 50px;
-    border-radius:30px;
-    text-align:left;
-    
+    .previewImage{
+        border-radius: 30px;
+    }
+    .grid {
+        display:grid;
+        grid-template-columns:1fr 1fr
+    }
+    .tile {
+        padding: 50px;
+        margin: 50px;
+        border-radius:30px;
+        text-align:left;
+        
 
-}
+    }
 
-.gridMobile {
-    display:grid;
-    grid-template-columns: 1
-}
+    .gridMobile {
+        display:grid;
+        grid-template-columns: 1
+    }
 
-.tileMobile {
-    padding: 25px;
-    margin: 25px;
-    border-radius:30px;
-    text-align:left;
-    border: 3px solid #0000F4;
-}
+    .tileMobile {
+        padding: 25px;
+        margin: 25px;
+        border-radius:30px;
+        text-align:left;
+        border: 3px solid #0000F4;
+    }
 </style>
--- src/pages/models/main.js
+++ src/pages/models/main.js
@@ -1,8 +1,70 @@
 import '../../app.css'
 import App from './App.svelte'
+import { onMount } from 'svelte';
 
 const app = new App({
   target: document.getElementById('app')
 })
 
-export default app
+// export default app
+// // JavaScript Source Code
+// // Original Author: PorkyProductions
+// // Language: javascript es6
+// // Path: js\models.js
+// $(document).ready(function () {
+//   OnStart();
+// });
+// //turns image name like books to images/books.png
+// function ImageNamesToFileNames(imageName) {
+//   return "images/" + imageName + ".png";
+// }
+// function ImageNamesArrayToFileNamesArray(imageNamesArray) {
+//   var fileNames = [];
+//   for (var i = 0; i < imageNamesArray.length; i++) {
+//       var imageName = imageNamesArray[i];
+//       var fileName = ImageNamesToFileNames(imageName);
+//       fileNames.push(fileName);
+//   }
+//   return fileNames;
+// }
+// var ImagePreview = /** @class */ (function () {
+//   function ImagePreview(ID, imageNames) {
+//       this.elementID = "#" + ID;
+//       this.files = ImageNamesArrayToFileNamesArray(imageNames);
+//       this.pointer = 0;
+//       this.length = this.files.length;
+//   }
+//   ImagePreview.prototype.GetCurrentImage = function () {
+//       return this.files[this.pointer];
+//   };
+//   ImagePreview.prototype.MovePointerRight = function () {
+//       this.pointer++;
+//       this.pointer = this.pointer % this.length;
+//   };
+//   ImagePreview.prototype.MovePointerLeft = function () {
+//       this.pointer--;
+//       if (this.pointer < 0) {
+//           this.pointer = this.length - 1;
+//       }
+//   };
+//   ImagePreview.prototype.OnSwitchRight = function () {
+//       this.MovePointerRight();
+//       $(this.elementID).attr("src", this.GetCurrentImage());
+//   };
+//   ImagePreview.prototype.OnSwitchLeft = function () {
+//       this.MovePointerLeft();
+//       $(this.elementID).attr("src", this.GetCurrentImage());
+//   };
+//   return ImagePreview;
+// }());
+// function OnStart() {
+//   //runs when page loads
+//   var HedgehogModelPreview = new ImagePreview("hedgehogModelPreview", ["HedgehogPreview_Front", "HedgehogPreview_Side", "HedgehogPreview_Top", "HedgehogPreview_Back"]);
+//   $("#button_HedgehogPreviewLeft").click(function () {
+//       HedgehogModelPreview.OnSwitchLeft();
+//   });
+//   $("#button_HedgehogPreviewRight").click(function () {
+//       HedgehogModelPreview.OnSwitchRight();
+//   });
+// }
+
