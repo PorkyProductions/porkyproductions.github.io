371e505acd53c45d318533bca4086241339012b3
HedgehogDubz <HedgehogDubz@users.noreply.github.com>
2022-08-14 20:44:52 -0400

    on hover for this thing

--- src/components/experimentalPageMain.svelte
+++ src/components/experimentalPageMain.svelte
@@ -8,7 +8,7 @@
     </h1>
     <div class="p-4 pl-32">incomplete or decidedly unfinished programs that we decided to still push out for the public to see!</div>
     <a href="https://porkyproductions.github.io/RayMarching/">
-        <div class="bg-gradient-to-r from-slate-300 to-slate-400 hover:bg-slate-600 p-10">
+        <div class="bg-gradient-to-r bg-slate-300 hover:bg-slate-600 p-10">
             <h1 class="text-center font-semibold p-24sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-primary">
                 Ray Marching
             </h1>
