d59a7d4bb7af3151e89316646e77bbebe3fcbf0c
HedgehogDubz <HedgehogDubz@users.noreply.github.com>
2022-08-15 13:39:15 -0400

    founders page has image of HEDGEHOG !!!!11!!1!1!!

--- src/components/mainPageOriginStory.svelte
+++ src/components/mainPageOriginStory.svelte
@@ -1,21 +1,23 @@
 <script>
     import '../app.css';
+    import hedgehogReading from '../../images/hedgehogReadingBook.jpeg'
 </script>
 <section>
-    <div class="purp-gradient">
+    <div class="bg-slate-800">
         <div class="p-20">
             <h1 class="text-center font-semibold text-white font-primary sm:text-lg md:text-xl lg:text-2xl xl:text-3xl p-6">
                 The PorkyProductions Origin Story
             </h1>
-            <p class="text-center text-white font-secondary sm:text-sm md:text-md lg:text-lg xl:text-xl px-10 pb-10">
+            <img src = {hedgehogReading} alt ="hedeghog reading a book" id="hedgehogReadingImage">
+            <p class="text-center text-white font-secondary sm:text-sm md:text-md lg:text-lg xl:text-xl px-10 pb-10 pt-10">
                 
                 Our story begins in 2017, when our founders, Tristan and Ryan met in school.
                 They became friends and eventually discorvered that they could make a website using the "Google Sites" service.
                 What better thing to make a website about than a hedgehog pencil sharpener that Tristan had brought to school and named "Porky".
                 After this website (PorkyPictures.inc) was created, many other websites followed such as PorkySchool.edu, PorkyPizza.eat, and <abbr title="PorkyBlackHolesThatWillDestroyTheEntireUniverseSoYouProbablyShouldntUseThe.com.com.inc">PBHTWDTEUSYPSUT.c.c.i</abbr>
-                At this point, Porky had developed a cult following. But then, sadly, a hater of porky stole the pencil sharpener, and destroyed it.
-                However, at the last moment, Porky transferred into a Hedgehog plush, where it resides to this day.
-                Later on, the old websites were archived, and consolidated into one company, called PorkyProductions, which is where we are today.
+                At this point, Porky had developed a cult following. But unfortunately, a disgusting hater of porky stole the pencil sharpener, and destroyed it.
+                During his last moments, Porky transferred into a Hedgehog plush, where he resides to this day.
+                Later on, the old websites were archived, and consolidated into one company, called PorkyProductions, which is the wonderful website you are visiting today.
             
             </p>
         </div>
@@ -25,12 +27,9 @@
 
 
 <style>
-.purp-gradient {
-    background: linear-gradient(90deg,hsl(341, 90%, 23%),hsl(343, 100%, 65%));
-    background-size: auto;
-    background-size: 400% 400%;
-    -webkit-animation: gradient 25s ease infinite;
-    animation: gradient 25s ease infinite;
+#hedgehogReadingImage{
+    border-radius: 30px;
+    border:20px solid brown;
 }
 @keyframes gradient {
     0% {
