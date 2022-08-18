060acbaaace4ebf87f2f7c92bc9b1c201a165dcb
hiteacheryouare <82683251+hiteacheryouare@users.noreply.github.com>
2022-08-17 20:50:27 -0400

    build: :building_construction: make test production build

---
+++ docs/assets/games.747a35e0.js
@@ -0,0 +1,17 @@
+import{S as v,i as y,s as g,h,d as c,n as m,g as d,e as k,b as $,D as f,a as _,F as u,l as b,t as p,H as x}from"./HedgehogIcon.2f8b33d2.js";import{D as M,H as w,F as E}from"./header.39480fcc.js";function R(o){let e;return{c(){e=k("section"),e.innerHTML=`<div class="grid svelte-57321r"><div class="tile drop-shadow-xl svelte-57321r"><b>RUSH!</b> - in with your friends and play this NEW SUPER AMAZING GREAT ULTRA MEGA GAME made by us called <b>RUSH!</b> a fun 2D fighter with a great campaign for anyone
+                            to enjoy! Make sure you check this one out! 
+                <br/> 
+                <hr/>
+                -HTML5 Game<br/>
+                -Windows Download<br/>
+                -Controller Support ONLY<br/>
+                -In Development<br/></div> 
+            <div class="tile drop-shadow-xl svelte-57321r"><h1 class="text-center font-bold font-primary sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl">UNDER CONSTRUCTION</h1></div></div>`,$(e,"class","section text-black dark:text-white dark:bg-black svelte-57321r")},m(r,s){c(r,e,s)},d(r){r&&d(e)}}}function A(o){let e;return{c(){e=k("section"),e.innerHTML=`<div class="gridMobile svelte-57321r"><div class="tileMobile svelte-57321r"><b>RUSH!</b> - in with your friends and play this NEW SUPER AMAZING GREAT ULTRA MEGA GAME made by us called <b>RUSH!</b> a fun 2D fighter with a great campaign for anyone
+                            to enjoy! Make sure you check this one out! 
+                <br/> 
+                <hr/>
+                -HTML5 Game<br/>
+                -Windows Download<br/>
+                -Controller Support ONLY<br/>
+                -In Development<br/></div> 
+            <div class="tileMobile svelte-57321r"><h1 class="text-center font-bold font-primary sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl">UNDER CONSTRUCTION</h1></div></div>`,$(e,"class","section text-black dark:text-white dark:bg-black svelte-57321r")},m(r,s){c(r,e,s)},d(r){r&&d(e)}}}function N(o){let e;function r(n,a){return M==="mobile"?A:R}let l=r()(o);return{c(){l.c(),e=h()},m(n,a){l.m(n,a),c(n,e,a)},p:m,i:m,o:m,d(n){l.d(n),n&&d(e)}}}class S extends v{constructor(e){super(),y(this,e,null,N,g,{})}}function T(o){let e,r,s,l,n,a;return e=new w({}),s=new S({}),n=new E({}),{c(){f(e.$$.fragment),r=_(),f(s.$$.fragment),l=_(),f(n.$$.fragment)},m(t,i){u(e,t,i),c(t,r,i),u(s,t,i),c(t,l,i),u(n,t,i),a=!0},p:m,i(t){a||(b(e.$$.fragment,t),b(s.$$.fragment,t),b(n.$$.fragment,t),a=!0)},o(t){p(e.$$.fragment,t),p(s.$$.fragment,t),p(n.$$.fragment,t),a=!1},d(t){x(e,t),t&&d(r),x(s,t),t&&d(l),x(n,t)}}}class U extends v{constructor(e){super(),y(this,e,null,T,g,{})}}new U({target:document.getElementById("app")});
