768e6b3f0b957a55e5a6aa04e0df62b1484395aa
hiteacheryouare <82683251+hiteacheryouare@users.noreply.github.com>
2022-08-17 20:41:30 -0400

    feat: :zap: made the 3D page highly interactive

--- src/components/modelsPageMain.svelte
+++ src/components/modelsPageMain.svelte
@@ -1,4 +1,5 @@
 <script>
+    // @ts-nocheck
     import '../app.css'
     import icelands from '../../images/icelands.png'
     import hedgehog from '../../images/HedgehogPreview_Front.png'
@@ -6,13 +7,13 @@
     import hedgehogModel from '../../models/hedgehog.stl'
     import bowModel from '../../models/Bow.stl'
     import { DEVICE_type } from 'uadetect'
-import ModelsTile from './modelsTile.svelte'
+    import ModelsTile from './modelsTile.svelte'
 </script>
 
 <!-- TODO: Make dark mode compatible -->
 
 {#if DEVICE_type === "mobile"}
-    <section class="section text-black dark:text-white dark:bg-black">
+    <section class="bg-gradient-to-b from-pink-600 to-rose-600 dark:bg-slate-400 text-black dark:text-white">
         <div class="gridMobile">
             <div class="tileMobile">
                 <h1 class="font-bold p-4 font-primary sm:text-md md:text-lg lg:text-xl xl:text-2xl">
@@ -21,7 +22,6 @@ import ModelsTile from './modelsTile.svelte'
                 <button class = "previewButton text-8xl">&lt;</button>
                 <img src={hedgehog} alt="hedgehogModel" class="previewImage p-4  w-3/4">   <!--ACTUAL IMAGE ACTUAL IMAGEACTUAL IMAGE ACTUAL IMAGEACTUAL IMAGE ACTUAL IMAGEACTUAL IMAGE ACTUAL IMAGE-->
                 <button class = "previewButton text-8xl">&gt;</button>
-
                 <p>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum iste molestias repellendus dolorum qui quia, iure fugiat in vel optio! Iusto, cumque distinctio delectus fuga repellat at accusamus quis repudiandae!
                 </p>
@@ -48,20 +48,16 @@ import ModelsTile from './modelsTile.svelte'
         </div>
     </section>
 {:else}
-    <section class="section text-black dark:text-white dark:bg-black bg-slate-300">
+    <section class="bg-gradient-to-b from-pink-600 to-rose-600 dark:from-slate-500 dark:to-slate-500 text-black dark:text-white bg-slate-300">
         <div class="grid">
-            <ModelsTile modelName = "Hedgehog" modelDownload={hedgehogModel} modelImage={hedgehog} id="hedgehogModelPreview"/>
-            <ModelsTile modelName = "Bow" modelDownload ={bowModel} modelImage = {bow} id = "bowModelPreview"/>
-            <ModelsTile modelName = "Iceland" modelDownload = {hedgehogModel} modelImage={icelands} id = "doesnt have one "/>
+            <ModelsTile modelName = "Hedgehog" modelDownload={hedgehogModel} iFrameTitle={"hedgehog"} iFrameSource={"https://my.spline.design/untitled-9b130a1005f56022c585fab65feabcf1/"} id="hedgehogModelPreview"/>
+            <ModelsTile modelName = "Bow" modelDownload ={bowModel} iFrameTitle={"Bow"} iFrameSource = {"https://my.spline.design/bow-25bd00f35d7812dea212e793b6fef34d/"} id = "bowModelPreview"/>
+            <ModelsTile modelName = "Iceland" modelDownload = {hedgehogModel} image={icelands} id = "doesnt have one "/>
         </div>
     </section>
 {/if}
 
 <style>
-    .section {
-        background-image: linear-gradient(#710627, #FF4D80);
- 
-    }
     .previewImage{
         border-radius: 30px;
         padding: 0;
