62ba41fddcd35ce9cdf7632f47438cc64f7b3e10
HedgehogDubz <HedgehogDubz@users.noreply.github.com>
2022-08-21 10:41:15 -0400

    did stuff

--- src/components/3DprintsPageMain.svelte
+++ src/components/3DprintsPageMain.svelte
@@ -7,7 +7,7 @@
 
 
 {#if DEVICE_type === "mobile"}
-    <section class="text-black dark:text-white dark:bg-black">
+    <section class="bg-gradient-to-b  from-UADGreen to-green-600 text-black dark:text-white dark:bg-black">
         <h1 class="text-center font-primary font-semibold sm:text-lg md:text-xl lg:text-2xl xl:text-3xl p-6">
             3D Prints
         </h1>
@@ -37,12 +37,12 @@
         </div>
     </section>
 {:else}
-    <section class="text-black dark:text-white dark:bg-black">
+    <section class="bg-gradient-to-b from-UADGreen to-green-600 text-black dark:text-white dark:bg-black">
         <h1 class="text-center font-primary font-semibold sm:text-lg md:text-xl lg:text-2xl xl:text-3xl p-6">
             3D Prints
         </h1>
         <div class="grid">
-            <div class="tile">
+            <div class="tile bg-slate-100">
                 <h2 class="font-semibold font-primary text-center sm:text-lg md:text-xl lg:text-2xl xl:text-3xl">
                     The Imperial Credit
                 </h2>
@@ -53,7 +53,7 @@
                     The Imperial Credit is a replacement for the existing monetary standard. It can eaisly be mass-produced due to it's 3D printed nature. Instead of relying on cryptography like cryptocurrency, it is a simple, easy to use, and secure payment system. It is the first currency to be created using 3D printing, and the first to be regulated by a company 
                 </p>
             </div>
-            <div class="tile">
+            <div class="tile bg-slate-100">
                     <h2 class="font-semibold font-primary text-center sm:text-lg md:text-xl lg:text-2xl xl:text-3xl">
                         Hedgehog
                     </h2>
@@ -69,6 +69,7 @@
 {/if}
 
 <style>
+   
     .grid {
         display:grid;
         grid-template-columns:1fr 1fr
@@ -77,8 +78,9 @@
         padding: 50px;
         margin: 50px;
         border-radius:30px;
-        text-align:left;
-        border: 3px solid #0000F4;
+        height: 800px;
+        text-align: center;
+        justify-content: center;
     }
     
     .gridMobile {
