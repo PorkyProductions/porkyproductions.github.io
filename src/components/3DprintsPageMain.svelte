0cf609effde9e3a7aebb802227060c82110a3718
HedgehogDubz <HedgehogDubz@users.noreply.github.com>
2022-08-21 21:48:27 -0400

    3d prints page looks good

--- src/components/3DprintsPageMain.svelte
+++ src/components/3DprintsPageMain.svelte
@@ -42,23 +42,23 @@
             3D Prints
         </h1>
         <div class="grid">
-            <div class="tile bg-slate-100">
+            <div class="tile bg-slate-100 drop-shadow-2xl">
                 <h2 class="font-semibold font-primary text-center sm:text-lg md:text-xl lg:text-2xl xl:text-3xl">
                     The Imperial Credit
                 </h2>
                 <div class="m-auto">
-                    <img src={impCredit} id="imp" alt="">
+                    <img src={impCredit} class= "m-auto"id="imp" alt="">
                 </div>
                 <p class="font-light font-secondary sm:text-sm md:text-md lg:text-md xl:text-lg">
                     The Imperial Credit is a replacement for the existing monetary standard. It can eaisly be mass-produced due to it's 3D printed nature. Instead of relying on cryptography like cryptocurrency, it is a simple, easy to use, and secure payment system. It is the first currency to be created using 3D printing, and the first to be regulated by a company 
                 </p>
             </div>
-            <div class="tile bg-slate-100">
+            <div class="tile bg-slate-100 drop-shadow-2xl">
                     <h2 class="font-semibold font-primary text-center sm:text-lg md:text-xl lg:text-2xl xl:text-3xl">
                         Hedgehog
                     </h2>
                     <div class="m-auto">
-                        <img src={hedehog3D} id="imp" alt="">
+                        <img src={hedehog3D}  class= "m-auto"id="imp" alt="">
                     </div>
                     <p class="font-light font-secondary sm:text-sm md:text-md lg:text-md xl:text-lg">
                         The Hedgehog is a 3D printed model of our mascot, Porky. It is a small, cute, and cute. It is a hedgehog, and it is a hedgehog.
