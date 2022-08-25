a65ae6ab6d586e464725bb2243ddc3c0348856d6
HedgehogDubz <HedgehogDubz@users.noreply.github.com>
2022-08-25 11:03:55 -0400

    added new experimental boids

--- src/components/experimentalPageMain.svelte
+++ src/components/experimentalPageMain.svelte
@@ -26,11 +26,20 @@
     <a href="https://porkyproductions.github.io/RayMarching/">
         <div class="bg-gradient-to-r bg-slate-300 hover:bg-slate-600 dark:bg-slate-600 dark:hover:bg-slate-900 p-10">
             <h1 class="text-center font-semibold p-24sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-primary">
+                Boids
+            </h1>
+            <p class="text-center font-normal p-4 sm:text-lg md:text-xl lg:text-2xl xl:text-3xl">simple behaviours combined can create complex behaviours</p>
+        </div>  
+    </a>
+    <a href="https://porkyproductions.github.io/Boids/">
+        <div class="bg-gradient-to-r bg-slate-300 hover:bg-slate-600 dark:bg-slate-600 dark:hover:bg-slate-900 p-10">
+            <h1 class="text-center font-semibold p-24sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-primary">
                 Ray Marching
             </h1>
             <p class="text-center font-normal p-4 sm:text-lg md:text-xl lg:text-2xl xl:text-3xl">using a series of distance functions, we can conclude if and where a ray collides with certain objects</p>
         </div>  
     </a>
+    
 </section>
 
 <style>
